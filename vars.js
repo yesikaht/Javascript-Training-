3bae398fd9ba3eb3b4afb6647bac62e0eb70e630
Yesika <yesikahinojosa@gmail.com>
2022-04-09 13:10:46 -0500

    variables training

---
+++ vars.js
@@ -0,0 +1,20 @@
+//I am going to sum 2 numbers yeeeeei 
+/**
+ * es bueno comentar siempre. aunque mucha gene diga que no
+ * pero esa es gente boba, dicen falacias. 
+ */
+let x = 5; 
+let y = 9;
+x + y 
+console.log(x + y);
+/**
+ * funcion que reciba como parametro una palabra 
+ *(nombre) y retorne un saludo. ej. hola amigo. hola yesika
+ hola alguien */
+ let nombre = "yesika";
+ function saying_hi(me) {
+    return "hola".concat(" ", me)
+  }
+console.log(saying_hi(nombre))
+// variables y funciones en minuscula 
+//          SIEMPREEEEEE *o*
